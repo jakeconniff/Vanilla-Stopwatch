eed849438d3467b002e8eb5e15e78f90cefea629
jakeconniff <54566903+jakeconniff@users.noreply.github.com>
2021-06-27 01:05:54 -0500

    JS annotated

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+//button references for HTML manipulation
 const startButton = document.querySelector('#start-button')
 const stopButton = document.querySelector('#stop-button')
 const resetButton = document.querySelector('#reset-button')
 
+//time field references
 const timeSection = document.querySelector('.timer')
 const hourSection = document.querySelector('#hours-span')
 const minuteSection = document.querySelector('#minutes-span')
 const secondSection = document.querySelector('#seconds-span')
 
+//application variables
 var hours = 0
 var minutes = 0
 var seconds = 0
@@ -22,24 +25,29 @@ function startTimer(){
 function stopTimer(){
     console.log('Stop timer')
 
+    //pause interval
     clearInterval(timeInterval)
 }
 
 function resetTimer(){
+    //stop interval
     clearInterval(timeInterval)
 
+    //reset external times
     hourSection.innerHTML = "00"
     minuteSection.innerHTML = "00"
     secondSection.innerHTML = "00"
 
     console.log(`Reset at ${hours}:${minutes}:${seconds}`)
 
+    //reset internal times
     hours = 0
     minutes = 0
     seconds = 0
 }
 
 function timer(){
+    //updating internal clock
     if(seconds === 59){
         seconds = 0
         if(minutes === 59){
@@ -54,6 +62,7 @@ function timer(){
         seconds++
     }
 
+    //updating external clock
     secondSection.innerHTML = (seconds<10) ? "0" + seconds : seconds
     minuteSection.innerHTML = (minutes<10) ? "0" + minutes : minutes
     hourSection.innerHTML = (hours<10) ? "0" + hours : hours
